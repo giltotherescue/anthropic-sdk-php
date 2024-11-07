4feae73db36623fdcb04e40fba401348eaf16f41
Gil Hildebrand <gilhildebrand@gmail.com>
2024-11-07 16:36:22 -0600

    Log requests

--- src/Client.php
+++ src/Client.php
@@ -15,10 +15,59 @@ class Client
 
     public function __construct(private string $baseUrl, private array $headers = [])
     {
+        $stack = \GuzzleHttp\HandlerStack::create();
+        if (class_exists('\Illuminate\Support\Facades\Log')) {
+            $stack->push($this->createLoggingMiddleware());
+        }
+
         $this->client = new GuzzleClient([
             'base_uri' => $this->baseUrl,
             'headers' => $headers,
+            'handler' => $stack,
         ]);
+    }
+
+    private function createLoggingMiddleware(): callable
+    {
+        return function (callable $handler) {
+            return function ($request, array $options) use ($handler) {
+                $start = microtime(true);
+                
+                return $handler($request, $options)->then(
+                    function ($response) use ($request, $start, $options) {
+                        $duration = microtime(true) - $start;
+                        
+                        $context = [
+                            'anthropic' => true,
+                            'method' => $request->getMethod(),
+                            'uri' => (string) $request->getUri(),
+                            'headers' => $request->getHeaders(),
+                            'body' => json_decode((string) $request->getBody(), true),
+                            'duration' => round($duration * 1000, 2) . 'ms',
+                            'response_status' => $response->getStatusCode(),
+                            'response_headers' => $response->getHeaders(),
+                            'response_body' => !isset($options['stream']) ? 
+                                json_decode((string) $response->getBody(), true) : 
+                                '[STREAM]'
+                        ];
+
+                        \Illuminate\Support\Facades\Log::debug('Anthropic API Request', $context);
+
+                        return $response;
+                    },
+                    function ($reason) use ($request) {
+                        \Illuminate\Support\Facades\Log::error('Anthropic API Error', [
+                            'anthropic' => true,
+                            'method' => $request->getMethod(),
+                            'uri' => (string) $request->getUri(),
+                            'error' => $reason->getMessage()
+                        ]);
+                        
+                        throw $reason;
+                    }
+                );
+            };
+        };
     }
 
     public function post(string $endpoint, array $args, array $extraHeaders = []): ResponseInterface|ErrorResponse
